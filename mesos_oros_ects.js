5102d6443caee73dcd64a169cf8e3eade22b37f9
NohponeX <NohponeX@gmail.com>
2014-06-21 00:08:11 +0300

    Update mesos_oros_ects.js

--- mesos_oros_ects.js
+++ mesos_oros_ects.js
@@ -1,35 +1,39 @@
 // ==UserScript==
 // @name        mesos_oros_ects
-// @namespace   nohponex
+// @namespace   https://github.com/NohponeX/mesos_oros_ects
+// @author      NohponeX
 // @description Υπολογισμός (νέου) μέσου όρου βάση των μονάδων ECTS στην ιστοσελίδα του ITC για το auth.gr
 // @include     https://sw*.eng.auth.gr/unistudent/stud_CResults.asp?studPg=1*
 // @version     1
 // @grant       none
+// @date        21.06.2014
 // ==/UserScript==
 
 //Include jQuery if not included
+function getScript(url, success) {
+    var script = document.createElement('script');
+    script.src = url;
+    var head = document.getElementsByTagName('head') [0],
+    done = false;
+    // Attach handlers for all browsers
+    script.onload = script.onreadystatechange = function () {
+        if (!done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) {
+            done = true;
+            // callback function provided as param
+            success();
+            script.onload = script.onreadystatechange = null;
+            head.removeChild(script);
+        }
+    };
+    head.appendChild(script);
+}
+    
 if (typeof jQuery == 'undefined') {
     if (typeof $ == 'function') {
         // warning, global var
         thisPageUsingOtherJSLibrary = true;
     }
-    function getScript(url, success) {
-        var script = document.createElement('script');
-        script.src = url;
-        var head = document.getElementsByTagName('head') [0],
-        done = false;
-        // Attach handlers for all browsers
-        script.onload = script.onreadystatechange = function () {
-            if (!done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) {
-                done = true;
-                // callback function provided as param
-                success();
-                script.onload = script.onreadystatechange = null;
-                head.removeChild(script);
-            };
-        };
-        head.appendChild(script);
-    };
+    
     getScript('//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js', function () {
         if (typeof jQuery == 'undefined') {
             // Super failsafe - still somehow failed...
@@ -40,7 +44,7 @@ if (typeof jQuery == 'undefined') {
     });
 } else {
     mesos_oros_ects();
-};
+}
 
 //Main function
 function mesos_oros_ects() {
