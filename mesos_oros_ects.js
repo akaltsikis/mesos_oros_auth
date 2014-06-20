2c87d73af796b08ac9c3503c1a5650bb69bffb19
NohponeX <NohponeX@gmail.com>
2014-06-21 00:37:42 +0300

    Update mesos_oros_ects.js

--- mesos_oros_ects.js
+++ mesos_oros_ects.js
@@ -4,50 +4,41 @@
 // @author      NohponeX
 // @description Υπολογισμός (νέου) μέσου όρου βάση των μονάδων ECTS στην ιστοσελίδα του ITC για το auth.gr
 // @include     https://sw*.eng.auth.gr/unistudent/stud_CResults.asp?studPg=1*
-// @version     1
+// @version     2
 // @grant       none
 // @date        21.06.2014
 // ==/UserScript==
 
 //Include jQuery if not included
-function getScript(url, success) {
-    var script = document.createElement('script');
-    script.src = url;
-    var head = document.getElementsByTagName('head') [0],
-    done = false;
-    // Attach handlers for all browsers
-    script.onload = script.onreadystatechange = function () {
-        if (!done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) {
-            done = true;
-            // callback function provided as param
-            success();
-            script.onload = script.onreadystatechange = null;
-            head.removeChild(script);
-        }
+if (typeof jQuery == 'undefined') {    
+    function getScript(url, success) {
+        var script = document.createElement('script');
+        script.src = url;
+        var head = document.getElementsByTagName('head')[0],
+        done = false;
+        // Attach handlers for all browsers
+        script.onload = script.onreadystatechange = function () {
+            if (!done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) {
+                done = true;
+                // callback function provided as param
+                success();
+                script.onload = script.onreadystatechange = null;
+                head.removeChild(script);
+            };
+        };
+        head.appendChild(script);
     };
-    head.appendChild(script);
-}
-    
-if (typeof jQuery == 'undefined') {
-    if (typeof $ == 'function') {
-        // warning, global var
-        thisPageUsingOtherJSLibrary = true;
-    }
-    
     getScript('//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js', function () {
-        if (typeof jQuery == 'undefined') {
-            // Super failsafe - still somehow failed...
+        if (typeof jQuery == 'undefined'){
         } else {
-            // jQuery loaded! Make sure to use .noConflict just in case
             mesos_oros_ects();
         }
     });
 } else {
     mesos_oros_ects();
-}
-
+};
 //Main function
-function mesos_oros_ects() {
+function mesos_oros_ects(){
     //Get all tr with course data
     var items = $('#mainTable>tbody>tr:nth-child(2) table>tbody>tr[height="25"]:not(.italicHeader)');
     if( !items ){
@@ -83,4 +74,4 @@ function mesos_oros_ects() {
     } else {
         result_tr.append(' Νέος Μέσος Όρος (ECTS) : <span class="error">' + mean_ects + '</span>');
     }
-}
+};
